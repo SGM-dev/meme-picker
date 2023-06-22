8f7210c39614cc846067b930c0bf97add868c01e
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-22 10:57:18 +0530

    connect the "Get Image" button

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
+
+getImageBtn.addEventListener("click", getMatchingCatsArray);
 
 function highlightCheckedOption(e) {
   const radiosArray = document.getElementsByClassName("radio");
@@ -10,6 +13,13 @@ function highlightCheckedOption(e) {
     radio.classList.remove("highlight");
   }
   document.getElementById(e.target.id).parentElement.classList.add("highlight");
+}
+
+function getMatchingCatsArray() {
+  const selectedEmotion = document.querySelector(
+    "input[type=radio]:checked"
+  ).value;
+  console.log(selectedEmotion);
 }
 
 function getEmotionsArray(cats) {

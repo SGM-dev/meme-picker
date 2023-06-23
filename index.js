b7b4294d2a5f9155004eceb5366c149874bb2846
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-23 10:20:13 +0530

    render cat meme into modal

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
-const getImageBtn = document.getElementById("get-image-btn");
 const gifsOnlyOption = document.getElementById("gifs-only-option");
+const getImageBtn = document.getElementById("get-image-btn");
+const memeModal = document.getElementById("meme-modal");
+const memeModalInner = document.getElementById("meme-modal-inner");
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
 
@@ -46,7 +48,12 @@ function getSingleCatObject() {
 }
 
 function renderCat() {
-  getSingleCatObject();
+  const catObject = getSingleCatObject();
+  memeModalInner.innerHTML = `<img class="cat-img"
+    src="./images/${catObject.image}"
+    alt="${catObject.alt}"
+  />`;
+  memeModal.style.display = "flex";
 }
 
 function getEmotionsArray(cats) {
